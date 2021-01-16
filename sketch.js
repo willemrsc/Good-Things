b7e01d989297165ecb5307cdfbc0aca9fc265da7
willemrsc <62255581+willemrsc@users.noreply.github.com>
2021-01-16 16:45:06 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,23 +9,50 @@ function draw() {
   noErase();
   fill(235, 64, 52);
 
- var janEleven = {
-   a : 'ate tomatoes',
-   b : 'went to schaller and weber',
-   c : 'game of thrones',
- };
+var date = [
 
- var janTwelve = {
-   a : 'called the banana phone',
-   b : 'baked bread',
-   c : 'coded this site',
- };
+  '01.15.21',
+  '01.14.21',
+  '01.13.21',
+  '01.12.21',
+  '01.11.21',
 
-var first = [janEleven.a, janTwelve.a];
-var second = [janEleven.b, janTwelve.b];
-var third = [janEleven.c, janTwelve.c];
+];
 
+var first = [
+
+  'Austin got an internship at slack',
+  'made potstickers for lunch',
+  'experimented with liming oak',
+  'called the banana phone',
+  'ate tomatoes',
+
+];
+
+var second = [
+
+  'ate excellent pizza',
+  'caught up with Soren',
+  'made pupusas',
+  'baked bread',
+  'went to schaller and weber',
+
+];
+var third = [
+
+  'had an Austrian beer',
+  'made a spreadsheet',
+  'talked with dad',
+  'coded this site',
+  'game of thrones',
+
+];
+
+  fill(235,64,52);
   textSize(48);
+  rectMode(CORNER);
+  textAlign(LEFT,TOP);
+  text(date[index],20,20, windowWidth/4, windowHeight/4);
   rectMode(CENTER);
   textAlign(CENTER,CENTER);
   text(first[index],windowWidth/2,windowHeight/2-windowHeight/4, windowWidth/4, windowHeight/4);
